661271a637a11a36953001dda6401b3f26705c38
aswathnaray <aswathnaray@gmail.com>
2017-07-14 11:40:19 +0200

    Update Send_text.py

--- Local_run/Udacity_Python/Send_text.py
+++ Local_run/Udacity_Python/Send_text.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC88721a7d8bae5a7370df4e7e04980c9f"
+account_sid = 
 # Your Auth Token from twilio.com/console
-auth_token  = "3a77a307bbf4f4cf655d14e8565fdd72"
+auth_token  = 
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+32489466924",
-    from_="+32460208467",
-    body="Hello Bhave, this is an automated message from a python code!")
+    to=
+    from_=
+    body=
 
 print(message.sid)
